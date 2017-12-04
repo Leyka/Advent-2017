f082705ab2217db32bb89de8c514059a5884f574
Skander <skander.kc@gmail.com>
2017-12-04 00:58:28 -0500

    Day 4 done

---
+++ Day 4/passphrases.py
@@ -0,0 +1,30 @@
+# Day 4: High-Entropy Passphrases
+# http://adventofcode.com/2017/day/4
+
+def check_unique_passphrases(words, check_anagrams=False): 
+    unique_words = []
+    valid = True
+    for word in words:
+        # Part two
+        if check_anagrams:
+            word = ''.join(sorted(word)) # sort letters a->z
+        if word not in unique_words:
+            unique_words.append(word)
+        else:
+            valid = False
+            break
+    return valid
+
+def main():
+    total_one = 0
+    total_two = 0
+    with open('input.txt', 'r') as file: 
+        for row in file: 
+            words = row.split()
+            total_one +=  check_unique_passphrases(words)
+            total_two += check_unique_passphrases(words, True)
+    print('Part one: ' + str(total_one))
+    print('Part two: ' + str(total_two))
+
+if __name__ == '__main__': 
+    main()
